e629a84059ceafe4b9a3ee79e6730f31a625db11
Bryan Cusme <bryan.cusme@devoteam.com>
2023-06-23 12:19:28 +0200

    refact: express app

---
+++ server/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+import cors from "cors"
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(cors);
+export const startServer = async () =>{
+    app.listen(PORT, () => {
+    
+        console.log(`Server listening on port ${PORT}` )
+    })
+}
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
-import express from "express";
-import cors from "cors"
 import dotenv from "dotenv"
 import {dbConnect} from "../config/mongo"
-dotenv.config()
-const PORT = process.env.PORT || 3001;
+import {startServer} from "../server/index"
 
-const app = express();
-app.use(cors);
-app.listen(PORT, () => {
-    
-    console.log(`Server listening on port ${PORT}` )
-})
+dotenv.config()
+
+startServer()
+
 dbConnect()
