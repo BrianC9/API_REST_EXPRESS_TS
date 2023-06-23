82f6b87b1828bd74ca51c1d6909697062e0886b1
Bryan Cusme <bryan.cusme@devoteam.com>
2023-06-23 11:21:15 +0200

    feat:testing express server

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors"
+import dotenv from "dotenv"
 
-const PORT = 3000
-const app = express()
+dotenv.config()
+const PORT = process.env.PORT || 3001;
 
+const app = express();
+app.use(cors);
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}` )
 })
